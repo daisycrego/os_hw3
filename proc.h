75e8c85f959bfed854796b2ee4e892ab60955193
Daisy Crego <daisycrego@gmail.com>
2019-04-13 19:55:18 -0400

    Adding numTickets to struct proc and numTicketsTotal to struct cpu.

--- proc.h
+++ proc.h
@@ -10,7 +10,8 @@ struct cpu {
   volatile uint started;       // Has the CPU started?
   int ncli;                    // Depth of pushcli nesting.
   int intena;                  // Were interrupts enabled before pushcli?
-  
+  int totalNumTickets;         // Total number of tickets awarded across all processes (for lottery scheduling)
+
   // Cpu-local storage variables; see below
   struct cpu *cpu;
   struct proc *proc;           // The currently-running process.
@@ -66,6 +67,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int numTickets;              // Number of tickets (for lottery scheduling)
 };
 
 // Process memory is laid out contiguously, low addresses first:
